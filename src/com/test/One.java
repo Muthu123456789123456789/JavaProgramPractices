8b82ddeb4b801adaa79a86501f8d7c8e2207aaec
Muthu123456789123456789 <muthuraja20111311@gmail.com>
2025-08-05 09:36:57 +0530

    init

--- src/com/test/One.java
+++ src/com/test/One.java
@@ -27,7 +27,7 @@ public class One {
 	                System.out.println(entry.getKey() + " = " + entry.getValue() + " times");
 	            }
 	            
-	            System.out.println(entry.getKey() + " = " + entry.getValue() + " times");
+	           
 	        }
 	    }
 
